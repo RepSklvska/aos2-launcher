7c47699aaff0a1e53b46bf522c5aff3c6e29b6e8
Iztecteocuitlatl Sokolov <gunpowderfans@mail.ru>
2024-07-14 05:01:59 +0800

    init commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,128 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.Label1 = new System.Windows.Forms.Label();
+			this.ButtonSetVersion = new System.Windows.Forms.Button();
+			this.ButtonVersionInfo = new System.Windows.Forms.Button();
+			this.ComboBoxVersionSelect = new System.Windows.Forms.ComboBox();
+			this.ButtonAbout = new System.Windows.Forms.Button();
+			this.ButtonPlay = new System.Windows.Forms.Button();
+			this.LabelCurrentVersion = new System.Windows.Forms.Label();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.LabelCurrentVersion);
+			this.groupBox1.Controls.Add(this.Label1);
+			this.groupBox1.Controls.Add(this.ButtonSetVersion);
+			this.groupBox1.Controls.Add(this.ButtonVersionInfo);
+			this.groupBox1.Controls.Add(this.ComboBoxVersionSelect);
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(172, 99);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Version Selection";
+			// 
+			// Label1
+			// 
+			this.Label1.AutoSize = true;
+			this.Label1.Location = new System.Drawing.Point(7, 20);
+			this.Label1.Name = "Label1";
+			this.Label1.Size = new System.Drawing.Size(44, 13);
+			this.Label1.TabIndex = 3;
+			this.Label1.Text = "Current:";
+			// 
+			// ButtonSetVersion
+			// 
+			this.ButtonSetVersion.Location = new System.Drawing.Point(91, 70);
+			this.ButtonSetVersion.Name = "ButtonSetVersion";
+			this.ButtonSetVersion.Size = new System.Drawing.Size(75, 23);
+			this.ButtonSetVersion.TabIndex = 2;
+			this.ButtonSetVersion.Text = "Set Version";
+			this.ButtonSetVersion.UseVisualStyleBackColor = true;
+			// 
+			// ButtonVersionInfo
+			// 
+			this.ButtonVersionInfo.Location = new System.Drawing.Point(6, 70);
+			this.ButtonVersionInfo.Name = "ButtonVersionInfo";
+			this.ButtonVersionInfo.Size = new System.Drawing.Size(75, 23);
+			this.ButtonVersionInfo.TabIndex = 1;
+			this.ButtonVersionInfo.Text = "Info";
+			this.ButtonVersionInfo.UseVisualStyleBackColor = true;
+			// 
+			// ComboBoxVersionSelect
+			// 
+			this.ComboBoxVersionSelect.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.ComboBoxVersionSelect.FormattingEnabled = true;
+			this.ComboBoxVersionSelect.Location = new System.Drawing.Point(6, 42);
+			this.ComboBoxVersionSelect.Name = "ComboBoxVersionSelect";
+			this.ComboBoxVersionSelect.Size = new System.Drawing.Size(160, 21);
+			this.ComboBoxVersionSelect.TabIndex = 0;
+			this.ComboBoxVersionSelect.SelectedIndexChanged += new System.EventHandler(this.ComboBoxVersionSelect_SelectedIndexChanged);
+			// 
+			// ButtonAbout
+			// 
+			this.ButtonAbout.Location = new System.Drawing.Point(12, 117);
+			this.ButtonAbout.Name = "ButtonAbout";
+			this.ButtonAbout.Size = new System.Drawing.Size(81, 23);
+			this.ButtonAbout.TabIndex = 1;
+			this.ButtonAbout.Text = "About";
+			this.ButtonAbout.UseVisualStyleBackColor = true;
+			// 
+			// ButtonPlay
+			// 
+			this.ButtonPlay.Location = new System.Drawing.Point(103, 117);
+			this.ButtonPlay.Name = "ButtonPlay";
+			this.ButtonPlay.Size = new System.Drawing.Size(81, 23);
+			this.ButtonPlay.TabIndex = 2;
+			this.ButtonPlay.Text = "Play";
+			this.ButtonPlay.UseVisualStyleBackColor = true;
+			// 
+			// LabelCurrentVersion
+			// 
+			this.LabelCurrentVersion.AutoSize = true;
+			this.LabelCurrentVersion.Location = new System.Drawing.Point(49, 20);
+			this.LabelCurrentVersion.Name = "LabelCurrentVersion";
+			this.LabelCurrentVersion.Size = new System.Drawing.Size(31, 13);
+			this.LabelCurrentVersion.TabIndex = 4;
+			this.LabelCurrentVersion.Text = "0.0.0";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(196, 150);
+			this.Controls.Add(this.ButtonPlay);
+			this.Controls.Add(this.ButtonAbout);
+			this.Controls.Add(this.groupBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.ShowIcon = false;
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+			this.Text = "AOS2 Version Selector";
+			this.TopMost = true;
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Button ButtonSetVersion;
+		private System.Windows.Forms.Button ButtonVersionInfo;
+		private System.Windows.Forms.ComboBox ComboBoxVersionSelect;
+		private System.Windows.Forms.Button ButtonAbout;
+		private System.Windows.Forms.Button ButtonPlay;
+		private System.Windows.Forms.Label Label1;
+		private System.Windows.Forms.Label LabelCurrentVersion;
 	}
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,51 @@
-﻿using System;
+﻿using LibGit2Sharp;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace aos2_launcher
 {
 	public partial class Form1 : Form
 	{
+		public Repository repo = new Repository(@"C:\Users\a\Desktop\aos2-all-versions");
+		public Dictionary<string, Branch> branches = new Dictionary<string, Branch>();
+		public IList<string> versions = new List<string>();
+		public string selectedVersion = "";
+		public string currentVersion = "";
 		public Form1()
 		{
+			versions.Add("1.6.2");
+			versions.Add("1.6.3b");
+			versions.Add("1.6.4");
+			versions.Add("1.7.6");
+			versions.Add("1.8.4");
+			versions.Add("1.9");
+
 			InitializeComponent();
+
+			currentVersion = repo.Head.FriendlyName;
+			selectedVersion = currentVersion;
+			LabelCurrentVersion.Text = currentVersion;
+			ComboBoxVersionSelect.DataSource = versions;
+			ComboBoxVersionSelect.SelectedIndex = versions.IndexOf(currentVersion);
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			Console.WriteLine(System.Environment.CurrentDirectory);
+			foreach (var branch in repo.Branches)
+			{
+				Console.WriteLine("Branch: " + branch.FriendlyName);
+				branches.Add(branch.FriendlyName, branch);
+			};
+			//Commands.Checkout(repo, branches["1.6.2"]);
+		}
+
+		private void ComboBoxVersionSelect_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			ComboBox comboBox = (ComboBox)sender;
+			selectedVersion = versions[comboBox.SelectedIndex];
+
 		}
 	}
 }
